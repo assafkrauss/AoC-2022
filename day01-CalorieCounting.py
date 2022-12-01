012525b2699a2aa956d8beffd20138b8ce2ee76d
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-01 23:01:59 +0200

    Day 1 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day01-CalorieCounting.py
+++ day01-CalorieCounting.py
@@ -2,20 +2,35 @@ def main():
     f = open("day01.txt")
     lines = f.readlines()
 
-    max_count = 0
+    top3 = [0, 0, 0]
     current_count = 0
     for i in range(len(lines)):
         if len(lines[i].strip()) == 0:
-            if current_count > max_count:
-                max_count = current_count
+            if current_count > top3[0]:
+                top3[2] = top3[1]
+                top3[1] = top3[0]
+                top3[0] = current_count
+            elif current_count > top3[1]:
+                top3[2] = top3[1]
+                top3[1] = current_count
+            elif current_count > top3[2]:
+                top3[2] = current_count
             current_count = 0
             continue
         current_count += int(lines[i].strip())
     # counting for that last elf:
-    if current_count > max_count:
-        max_count = current_count
+    if current_count > top3[0]:
+        top3[2] = top3[1]
+        top3[1] = top3[0]
+        top3[0] = current_count
+    elif current_count > top3[1]:
+        top3[2] = top3[1]
+        top3[1] = current_count
+    elif current_count > top3[2]:
+        top3[2] = current_count
 
-    print(max_count)
+    print("#1: {0}".format(top3[0]))
+    print("top 3 sum: {0}".format(sum(top3)))
 
 
 if __name__ == '__main__':
