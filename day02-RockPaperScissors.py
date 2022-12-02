3d23d45e9b868248d9e9de0bee9cf2b3e09c2155
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-02 09:21:04 +0200

    Day 2 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day02-RockPaperScissors.py
+++ day02-RockPaperScissors.py
@@ -1,4 +1,7 @@
 shape_score_sheet = {'X': 1, 'Y': 2, 'Z': 3}
+pick_to_win = {'A': 'Y', 'B': 'Z', 'C': 'X'}
+pick_to_lose = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+pick_to_draw = {'A': 'X', 'B': 'Y', 'C': 'Z'}
 
 
 def rps_round(his, mine):
@@ -10,6 +13,16 @@ def rps_round(his, mine):
     return score + 6
 
 
+def rps_round_part2(his, outcome):
+    if outcome == 'X':
+        mine = pick_to_lose[his]
+    elif outcome == 'Y':
+        mine = pick_to_draw[his]
+    else:
+        mine = pick_to_win[his]
+    return rps_round(his, mine)
+
+
 def main():
     f = open("day02.txt")
     lines = f.readlines()
@@ -18,7 +31,12 @@ def main():
     for line in lines:
         parts = line.split()
         total += rps_round(parts[0], parts[1])
+    print(total)
 
+    total = 0
+    for line in lines:
+        parts = line.split()
+        total += rps_round_part2(parts[0], parts[1])
     print(total)
 
 
