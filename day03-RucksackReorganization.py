cea7ad80e712e2032923347b84c159b968e63e24
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-03 21:00:39 +0200

    Day 3 - 1st star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

---
+++ day03-RucksackReorganization.py
@@ -0,0 +1,20 @@
+def main():
+    f = open("day03.txt")
+    lines = f.readlines()
+
+    total_priority = 0
+    for line in lines:
+        stripped = line.strip()
+        part1 = stripped.strip()[:len(stripped)//2]
+        part2 = stripped.strip()[len(stripped)//2:]
+        if len(part2) != len(part1):
+            raise Exception("unexpected line length")
+        for c in part1:
+            if c in part2:
+                total_priority += ord(c) - ((97 - 1) if c.islower() else (65 - 27))
+                break
+    print(total_priority)
+
+
+if __name__ == '__main__':
+    main()
