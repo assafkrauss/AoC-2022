bdce0c5d1b7068c85b29f34c762e5c361bcd8291
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-05 21:27:22 +0200

    Day 5 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day05-SupplyStacks.py
+++ day05-SupplyStacks.py
@@ -21,23 +21,37 @@ def parse_crate_stacks(lines):
     return stacks
 
 
-def main():
-    f = open("day05.txt")
-    lines = f.readlines()
-
+def move_with_crate_mover(lines, is_model_9001):
     crate_stacks = parse_crate_stacks(lines)
     for line in lines:
         if not line.startswith("move"):
             continue
         line_parts = line.strip().split()
         n, org, dest = int(line_parts[1]), int(line_parts[3]), int(line_parts[5])
-        for i in range(n):
-            crate_stacks[dest].append(crate_stacks[org].pop())
+        if is_model_9001:
+            temp_stack = deque()
+            for i in range(n):
+                temp_stack.append(crate_stacks[org].pop())
+            for i in range(n):
+                crate_stacks[dest].append(temp_stack.pop())
+        else:
+            for i in range(n):
+                crate_stacks[dest].append(crate_stacks[org].pop())
 
     message = []
     for i in range(1, len(crate_stacks)):
         message.append(crate_stacks[i].pop())
-    print("".join(message))
+    return "".join(message)
+
+
+def main():
+    f = open("day05.txt")
+    lines = f.readlines()
+
+    message = move_with_crate_mover(lines, False)
+    print(message)
+    message = move_with_crate_mover(lines, True)
+    print(message)
 
 
 if __name__ == '__main__':
