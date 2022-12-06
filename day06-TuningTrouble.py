4d2aac4e0e842c3f9ab87a4ecac787744131ac93
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-06 12:59:31 +0200

    Day 6 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day06-TuningTrouble.py
+++ day06-TuningTrouble.py
@@ -1,12 +1,19 @@
+def contains_duplicates(arr):
+    for elem in arr:
+        if arr.count(elem) > 1:
+            return True
+    return False
+
+
 def main():
     f = open("day06.txt")
     line = f.readline()
 
-    for i in range(3, len(line)):
-        if line[i] not in line[i - 3:i] and line[i - 1] not in line[i - 3:i - 1] \
-                      and line[i - 2] not in line[i - 3:i - 2]:
-            print(i + 1)
-            break
+    for limit in [4, 14]:
+        for i in range(limit, len(line)):
+            if not contains_duplicates(line[i - limit + 1:i + 1]):
+                print(i + 1)
+                break
 
 
 if __name__ == '__main__':
