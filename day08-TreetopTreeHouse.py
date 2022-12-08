408c3bbf0d1f7fd799ec023dd5e20d9b2d7a6927
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-08 18:42:53 +0200

    Day 8 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day08-TreetopTreeHouse.py
+++ day08-TreetopTreeHouse.py
@@ -7,46 +7,56 @@ def main():
         matrix.append(list(map(lambda t: int(t), line.strip())))
 
     count = 0
+    best_scenic_score = 0
     for y in range(len(matrix)):
         for x in range(len(matrix)):
-            # search up:
+            up_scene, down_scene, left_scene, right_scene = 0, 0, 0, 0
+            # look up:
             seen = True
+            added = False
             for i in range(y - 1, -1, -1):
+                up_scene += 1
                 if matrix[y][x] <= matrix[i][x]:
                     seen = False
                     break
-            if seen:
+            if seen and not added:
                 count += 1
-                continue
-            # search down:
+                added = True
+            # look down:
             seen = True
             for i in range(y + 1, len(matrix)):
+                down_scene += 1
                 if matrix[y][x] <= matrix[i][x]:
                     seen = False
                     break
-            if seen:
+            if seen and not added:
                 count += 1
-                continue
-            # search left:
+                added = True
+            # look left:
             seen = True
             for i in range(x - 1, -1, -1):
+                left_scene += 1
                 if matrix[y][x] <= matrix[y][i]:
                     seen = False
                     break
-            if seen:
+            if seen and not added:
                 count += 1
-                continue
-            # search right:
+                added = True
+            # look right:
             seen = True
             for i in range(x + 1, len(matrix)):
+                right_scene += 1
                 if matrix[y][x] <= matrix[y][i]:
                     seen = False
                     break
-            if seen:
+            if seen and not added:
                 count += 1
-                continue
+            scenic_score = up_scene * down_scene * left_scene * right_scene
+            if best_scenic_score < scenic_score:
+                best_scenic_score = scenic_score
 
     print(count)
+    print(best_scenic_score)
 
 
 if __name__ == '__main__':
