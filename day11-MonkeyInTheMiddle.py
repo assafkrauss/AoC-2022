18f50a39289f03231226dd0e2c10e518ed50c49e
Assaf Krauss <assaf.krauss@gmail.com>
2022-12-14 19:02:41 +0200

    Day 11 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day11-MonkeyInTheMiddle.py
+++ day11-MonkeyInTheMiddle.py
@@ -1,8 +1,71 @@
+day11_input = {
+    "monkeys": {
+        0: {"items": [85, 79, 63, 72],
+            "worry_up_func": lambda w: w * 17,
+            "test": lambda w: w % 2 == 0,
+            "target_if_true": 2,
+            "target_if_false": 6
+            },
+        1: {"items": [53, 94, 65, 81, 93, 73, 57, 92],
+            "worry_up_func": lambda w: w * w,
+            "test": lambda w: w % 7 == 0,
+            "target_if_true": 0,
+            "target_if_false": 2
+            },
+        2: {"items": [62, 63],
+            "worry_up_func": lambda w: w + 7,
+            "test": lambda w: w % 13 == 0,
+            "target_if_true": 7,
+            "target_if_false": 6
+            },
+        3: {"items": [57, 92, 56],
+            "worry_up_func": lambda w: w + 4,
+            "test": lambda w: w % 5 == 0,
+            "target_if_true": 4,
+            "target_if_false": 5
+            },
+        4: {"items": [67],
+            "worry_up_func": lambda w: w + 5,
+            "test": lambda w: w % 3 == 0,
+            "target_if_true": 1,
+            "target_if_false": 5
+            },
+        5: {"items": [85, 56, 66, 72, 57, 99],
+            "worry_up_func": lambda w: w + 6,
+            "test": lambda w: w % 19 == 0,
+            "target_if_true": 1,
+            "target_if_false": 0
+            },
+        6: {"items": [86, 65, 98, 97, 69],
+            "worry_up_func": lambda w: w * 13,
+            "test": lambda w: w % 11 == 0,
+            "target_if_true": 3,
+            "target_if_false": 7
+            },
+        7: {"items": [87, 68, 92, 66, 91, 50, 68],
+            "worry_up_func": lambda w: w + 2,
+            "test": lambda w: w % 17 == 0,
+            "target_if_true": 4,
+            "target_if_false": 3
+            }
+    },
+    "worry_down_func": {
+        1: lambda w: w // 3,
+        2: lambda w: w % (2 * 7 * 13 * 5 * 3 * 19 * 11 * 17)
+    },
+    "rounds": {
+        1: 20,
+        2: 10000
+    }
+}
+
+
 class Monkey:
-    def __init__(self, pack, items, worry_func, test, target_if_true, target_if_false):
+    def __init__(self, pack, items, worry_up_func, worry_down_func, test, target_if_true, target_if_false):
         self._pack = pack
         self._items = items
-        self._worry_func = worry_func
+        self._worry_up_func = worry_up_func
+        self._worry_down_func = worry_down_func
         self._test = test
         self._target_if_true = target_if_true
         self._target_if_false = target_if_false
@@ -14,8 +77,8 @@ class Monkey:
     def turn(self):
         self._inspections += len(self._items)
         for i in range(len(self._items)):
-            self._items[i] = self._worry_func(self._items[i])
-            self._items[i] //= 3
+            self._items[i] = self._worry_up_func(self._items[i])
+            self._items[i] = self._worry_down_func(self._items[i])
             if self._test(self._items[i]):
                 self._pack[self._target_if_true].add_item(self._items[i])
             else:
@@ -28,37 +91,30 @@ class Monkey:
 
 
 def main():
-    # noinspection PyListCreation
-    monkey_pack = []
-    # monkey_pack.append(Monkey(monkey_pack, [79, 98], lambda w: w * 19, lambda w: w % 23 == 0, 2, 3))
-    # monkey_pack.append(Monkey(monkey_pack, [54, 65, 75, 74], lambda w: w + 6, lambda w: w % 19 == 0, 2, 0))
-    # monkey_pack.append(Monkey(monkey_pack, [79, 60, 97], lambda w: w * w, lambda w: w % 13 == 0, 1, 3))
-    # monkey_pack.append(Monkey(monkey_pack, [74], lambda w: w + 3, lambda w: w % 17 == 0, 0, 1))
-    monkey_pack.append(Monkey(monkey_pack, [85, 79, 63, 72], lambda w: w * 17, lambda w: w % 2 == 0, 2, 6))
-    monkey_pack.append(Monkey(monkey_pack, [53, 94, 65, 81, 93, 73, 57, 92], lambda w: w * w,
-                              lambda w: w % 7 == 0, 0, 2))
-    monkey_pack.append(Monkey(monkey_pack, [62, 63], lambda w: w + 7, lambda w: w % 13 == 0, 7, 6))
-    monkey_pack.append(Monkey(monkey_pack, [57, 92, 56], lambda w: w + 4, lambda w: w % 5 == 0, 4, 5))
-    monkey_pack.append(Monkey(monkey_pack, [67], lambda w: w + 5, lambda w: w % 3 == 0, 1, 5))
-    monkey_pack.append(Monkey(monkey_pack, [85, 56, 66, 72, 57, 99], lambda w: w + 6, lambda w: w % 19 == 0, 1, 0))
-    monkey_pack.append(Monkey(monkey_pack, [86, 65, 98, 97, 69], lambda w: w * 13, lambda w: w % 11 == 0, 3, 7))
-    monkey_pack.append(Monkey(monkey_pack, [87, 68, 92, 66, 91, 50, 68], lambda w: w + 2, lambda w: w % 17 == 0, 4, 3))
+    for part in [1, 2]:
+        monkey_pack = []
+        for i in day11_input["monkeys"].keys():
+            monkey_pack.append(Monkey(monkey_pack, day11_input["monkeys"][i]["items"][:],
+                                      day11_input["monkeys"][i]["worry_up_func"],
+                                      day11_input["worry_down_func"][part],
+                                      day11_input["monkeys"][i]["test"],
+                                      day11_input["monkeys"][i]["target_if_true"],
+                                      day11_input["monkeys"][i]["target_if_false"]))
 
-    for r in range(20):
+        for r in range(day11_input["rounds"][part]):
+            for i in range(len(monkey_pack)):
+                monkey_pack[i].turn()
+
+        most_active_1, most_active_2 = 0, 0
         for monkey in monkey_pack:
-            monkey.turn()
-
-    most_active_1, most_active_2 = 0, 0
-    for monkey in monkey_pack:
-        inspections = monkey.inspections
-        if inspections > most_active_1:
-            most_active_2 = most_active_1
-            most_active_1 = inspections
-        elif inspections > most_active_2:
-            most_active_2 = inspections
-    monkey_business = most_active_1 * most_active_2
-
-    print(monkey_business)
+            inspections = monkey.inspections
+            if inspections > most_active_1:
+                most_active_2 = most_active_1
+                most_active_1 = inspections
+            elif inspections > most_active_2:
+                most_active_2 = inspections
+        monkey_business = most_active_1 * most_active_2
+        print(monkey_business)
 
 
 if __name__ == '__main__':
